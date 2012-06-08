0e512528fa7dd1993ec98d96e063711f16950135
pTalanov <talanov.pavel@gmail.com>
2012-06-08 17:04:29 +0400

    Rename module and move output to production directory

--- src/main.kt
+++ src/main.kt
@@ -6,12 +6,10 @@ import js.dom.html5.HTMLCanvasElement
 
 import js.dom.html.HTMLImageElement
 import js.dom.html.window
-import js.jquery.*
 import js.dom.html.HTMLElement
 
 fun main(args: Array<String>) {
-    window.onload = {
-        val canvas = window.document.getElementsByTagName("canvas").item(0)!! as HTMLCanvasElement
-        CanvasState(canvas).startDrawing()
-    }
+
+    val canvas = window.document.getElementsByTagName("canvas").item(0)!! as HTMLCanvasElement
+    CanvasState(canvas).startDrawing()
 }
